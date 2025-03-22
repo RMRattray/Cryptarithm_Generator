20f7410e21fad12b1617a85ec1279bb4853961d2
RMRattray <rmr3141@gmail.com>
2025-03-22 17:07:57 -0400

    Skeletal frontend

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -1,13 +1,72 @@
 #include "mainwindow.h"
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QScrollArea>
+#include <QLabel>
+#include <QComboBox>
+#include <QFormLayout>
+#include <QGroupBox>
 
 Window::Window(QWidget *parent) :
  QWidget(parent)
  {
  // Set size of the window
- setFixedSize(100, 50);
+ // setFixedSize(100, 90);
 
- // Create and position the button
- m_button = new QPushButton("Hello World", this);
- m_button->setGeometry(10, 10, 80, 30);
+ // General look of the main window:
+ /////////////////////////////////////////////////////
+ // Cryparithm Generator                     _ [] X //
+ /////////////////////////////////////////////////////
+ //                        //                       //
+ // Operation: [____Add_V] //  Solutions:           //
+ //                        //                       //
+ //          [___________] //                       //
+ //      [+] [___________] //                       //
+ //          ------------- //                       //
+ //          [___________] //                       //
+ /////////////////////////////////////////////////////
+
+ // Make a main layout object
+ main_split_layout = new QHBoxLayout;
+
+ // Split into two areas
+ solution_scroll_area = new QScrollArea;
+ left_stack_layout = new QVBoxLayout;
+ main_split_layout->addItem(left_stack_layout);
+ main_split_layout->addWidget(solution_scroll_area);
+
+ // First thing in the left-side stack
+ // are the dropdowns for operation
+ // and word count
+ operation_combo_box = new QComboBox();
+ operation_combo_box->addItem("addition");
+ operation_combo_box->addItem("subtraction");
+ operation_combo_box->addItem("multiplication");
+ word_count_combo_box = new QComboBox();
+ word_count_combo_box->addItem("single");
+ word_count_combo_box->addItem("multiple");
+
+ QFormLayout * selections = new QFormLayout();
+ selections->addRow(new QLabel(tr("Operation")), operation_combo_box);
+ selections->addRow(new QLabel(tr("Operation")), word_count_combo_box);
+
+ left_stack_layout->addItem(selections);
+ 
+ // Next thing is the arithmetic text area
+ 
+ // Last thing are the buttons to go
+ // and to reset
+
+ QHBoxLayout * button_area = new QHBoxLayout();
+ go_button = new QPushButton("GO");
+ reset_button = new QPushButton("Reset");
+ button_area->addWidget(reset_button, 1);
+ button_area->addWidget(go_button, 2);
+ button_area->addStretch(1);
+
+ left_stack_layout->addItem(button_area);
+
+ this->setLayout(main_split_layout);
+
 }
--- src/app/mainwindow.h
+++ src/app/mainwindow.h
@@ -4,12 +4,22 @@
 #include <QWidget>
 
 class QPushButton;
+class QVBoxLayout;
+class QHBoxLayout;
+class QScrollArea;
+class QComboBox;
 class Window : public QWidget
 {
  public:
   explicit Window(QWidget *parent = 0);
  private:
- QPushButton *m_button;
+ QComboBox *operation_combo_box;
+ QComboBox *word_count_combo_box;
+ QPushButton *reset_button;
+ QPushButton *go_button;
+ QHBoxLayout *main_split_layout;
+ QScrollArea *solution_scroll_area;
+ QVBoxLayout *left_stack_layout;
 };
 
 #endif // WINDOW_H
