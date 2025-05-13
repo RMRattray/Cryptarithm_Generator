c1af27381597ec23c499d9e7adc355f1e1538575
RMRattray <89876227+RMRattray@users.noreply.github.com>
2025-05-13 10:24:14 -0400

    Delete build folder (never should have been included)

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Cryptarithm_FILE C:/Users/robby/OneDrive/Documents/Cryptarithm/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Cryptarithm.exe)
-set(__QT_DEPLOY_TARGET_Cryptarithm_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Cryptarithm_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
